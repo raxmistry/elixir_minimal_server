3159d876ec4b5f50816c94504f823e1707eccce0
Rakesh Mistry <rakesh.mistry@gmail.com>
2019-05-18 16:14:01 +0200

    initial commit

---
+++ lib/minimal_server/endpoint.ex
@@ -0,0 +1,31 @@
+defmodule MinimalServer.Endpoint do 
+  use Plug.Router
+
+
+  plug(:match)
+
+  plug(Plug.Parsers,
+       parsers: [:json],
+       pass: ["application/json"],
+       json_decoder: Poison
+  )
+
+  plug(:dispatch)
+
+  forward("/bot", to: MinimalServer.Router)
+
+  match _ do 
+    send_resp(conn, 404, "Requested page cannot be found")
+  end
+
+
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]}
+    }
+  end
+
+  def start_link(_opts),
+    do: Plug.Adapters.Cowboy2.http(__MODULE__, [])
+end
